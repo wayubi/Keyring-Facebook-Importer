1903dfe0743bb5a7d85607b05e0a12eabb0585c0
W. Latif Ayubi <wayubi@gmail.com>
2021-06-22 19:38:32 -0700

    share image fix.

--- keyring-facebook-importer/keyring-importer-facebook.php
+++ keyring-facebook-importer/keyring-importer-facebook.php
@@ -532,7 +532,8 @@ function Keyring_Facebook_Importer() {
 							// $photos[] = $data->media->image->src;
 							$photos[] = $data->url;
 						} else if ($data->type == 'share') {
-							$photos[] = $data->media->image->src;
+							// $photos[] = $data->media->image->src;
+							$photos[] = $post->full_picture;
 						} else if ($data->type == 'animated_image_share') {
 							$photos[] = $post->link;
 						} else if ($data->type == 'native_templates') {
